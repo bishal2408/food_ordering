eb6288e8a1cf7e6648065e63777304a7064563e6
BISHAL LAMICHHANE <bishallamichhane24@gmail.com>
2021-12-09 21:32:57 +0545

    mybag designed

--- resources/views/order/mybag.blade.php
+++ resources/views/order/mybag.blade.php
@@ -5,52 +5,56 @@ order form
 @endsection
 
 @section('content')
-<div class="mybag-orders">
-    <h3>YOUR BAG ITEMS</h3>
-    <a href="{{ route('home') }}"> <button>Add more items</button> </a>
-    <table style="width: 50%;">
-        <tr>
-            <th style="text-align: left; padding: 8px;">Quantity</th>
-            <th style="text-align: left; padding: 8px;">Item</th>
-            <th style="text-align: left; padding: 8px;">Price per item</th>
-            <th style="text-align: left; padding: 8px;">Price</th>
-            <th style="text-align: left; padding: 8px;">Action</th>
-
-
-        </tr>
-        @foreach($orders as $order)
-        <tr>
-            <td style="text-align: left; padding: 8px;">{{ $order->quantity }}x</td>
-            <td style="text-align: left; padding: 8px;">{{ $order->food_name }} </td>
-            <td style="text-align: left; padding: 8px;">{{ $order->price}}</td>
-            <td style="text-align: left; padding: 8px;">{{ $order->price * $order->quantity}}</td>
-            <td style="text-align: left; padding: 8px;">
-                <a href="{{ route('order.editorder',['id'=>$order->id]) }}"><button>Edit</button></a>
-                <a href="{{ route('order.delete',['id'=>$order->id]) }}"><button>Delete</button></a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
+<div class="mybag">
+    <div class="mybag-orders">
+        <h3>MY BAG ITEMS</h3>
+        <table>
+            <tr>
+                <th style="text-align: left; padding: 8px;">Quantity</th>
+                <th style="text-align: left; padding: 8px;">Item</th>
+                <th style="text-align: left; padding: 8px;">Price per item</th>
+                <th style="text-align: left; padding: 8px;">Price</th>
+                <th style="text-align: left; padding: 8px;">Action</th>
+            </tr>
+            @foreach($orders as $order)
+            <tr class="table-data">
+                <td style="text-align: left; padding: 15px;">{{ $order->quantity }}x</td>
+                <td style="text-align: left; padding: 15px;">{{ $order->food_name }} </td>
+                <td style="text-align: left; padding: 15px;">{{ $order->price}}</td>
+                <td style="text-align: left; padding: 15px;">{{ $order->price * $order->quantity}}</td>
+                <td style="text-align: left; padding: 15px;">
+                    <a href="{{ route('order.editorder',['id'=>$order->id]) }}"><button class="btn edit-btn">Edit</button></a>
+                    <a href="{{ route('order.delete',['id'=>$order->id]) }}"><button class="btn delete-btn"> Delete</button></a>
+                </td>
+                <hr>
+            </tr>
+            @endforeach
+        </table>
+        <a href="{{ route('home') }}"> <button class="add-item">ADD MORE ITEMS + </button> </a>
+    </div>
+    <div class="mybag-bill" >
+        <p>YOUR BILL</p>
+        <hr style="margin-bottom: .5rem;">
+        <table>
+            <tr>
+                <td style="text-align: left; padding: 15px;"><b>SUB TOTAL</b></td>
+                <td style="text-align: left; padding: 15px;">NRs. {{ $total }}</td>
+            </tr>
+            <tr>
+                <td style="text-align: left; padding: 15px;"><b>VAT</b></td>
+                <td style="text-align: left; padding: 15px;">NRs. {{ $vat }}</td>
+            </tr>
+            <tr>
+                <td style="text-align: left; padding: 15px;"><b>DELIVERY CHARGE</b></td>
+                <td style="text-align: left; padding: 15px;">NRs. {{ $delivery_fee }}</td>
+            </tr>
+            <tr>
+                <td style="text-align: left; padding: 15px;"><b>GRAND TOTAL</b></td>
+                <td style="text-align: left; padding: 15px;">NRs. {{$total + $vat + $delivery_fee}}</td>
+            </tr>
+        </table>
+        <a href="{{ route('order.checkoutform') }}" ><button class="btn checkout-btn">Proceed to Checkout</button></a>
+    </div>
 </div>
-<div class="mybag-bill" style="margin-top: 50px;">
-    <table style=" width:40%;">
-        <tr>
-            <td style="text-align: left; padding: 8px;"><b>SUB TOTAL</b></td>
-            <td style="text-align: left; padding: 8px;">NRs. {{ $total }}</td>
-        </tr>
-        <tr>
-            <td style="text-align: left; padding: 8px;"><b>VAT</b></td>
-            <td style="text-align: left; padding: 8px;">NRs. {{ $vat }}</td>
-        </tr>
-        <tr>
-            <td style="text-align: left; padding: 8px;"><b>Delivery charge</b></td>
-            <td style="text-align: left; padding: 8px;">NRs. {{ $delivery_fee }}</td>
-        </tr>
-        <tr>
-            <td style="text-align: left; padding: 8px;"><b>Grand total</b></td>
-            <td style="text-align: left; padding: 8px;">NRs. {{$total + $vat + $delivery_fee}}</td>
-        </tr>
-    </table>
-    <a href="{{ route('order.checkoutform') }}" style="margin-left: 290px;"><button>Proceed to Checkout</button></a>
-</div>
+
 @endsection
